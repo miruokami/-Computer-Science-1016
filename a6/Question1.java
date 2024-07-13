f28f0bcb1abe64e16019672abf34482ccc7351ff
miruokami <118073418+miruokami@users.noreply.github.com>
2024-07-13 17:24:52 +0200

    Add files via upload

---
+++ a6/Question1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Question1
+{
+   public static void main ( String args [] )
+   {
+      System.out.println (new Shape (new Shape ("Pentagon", "Blue")));
+      System.out.println (new Circle (new Circle ("Circle", "Purple",  3)));
+      System.out.println (new Rectangle (new Rectangle ("Rectangle", "Red", 6, 8)));
+   }
+}
